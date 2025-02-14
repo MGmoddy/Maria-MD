127c0f57d169ab14b656996316adf81938bb6338
MGmoddy <nzekahm@gmail.com>
2025-02-14 14:20:25 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -8,8 +8,8 @@ global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.ownernumber = '919931122319'  //creator number
-global.ownername = 'Ayush' //owner name
+global.ownernumber = '+256777458543'  //creator number
+global.ownername = 'Mgtech' //owner name
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
