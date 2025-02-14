7c50260920cb62e4542c506b223d857db0f2119e
MGmoddy <nzekahm@gmail.com>
2025-02-14 14:49:22 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919931122319"
+global.ownernomer = "+256777458543"
 global.ownername = "Ayush pandey"
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.ownernumber = '919931122319'  //creator number
-global.ownername = 'Ayush' //owner name
+global.ownernumber = '+256777458543'  //creator number
+global.ownername = 'nzekah' //owner name
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
